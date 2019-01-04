3023d3785d94488fd07ffeeab6e1459789d49170
Lauren Reilly <LaurenReillyNet@gmail.com>
2019-01-04 13:57:29 -0500

    created a few components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Todos from './components/Todos'
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Brush Cats',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Cook Dinner',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Water Change',
+        completed: false
+      }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
