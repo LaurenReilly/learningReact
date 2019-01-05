76b2fefd9e1ce2735282e8729e37d1e9c3acd06e
Lauren Reilly <LaurenReillyNet@gmail.com>
2019-01-05 09:56:42 -0500

    connected todos props with todoitem props

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+
+import PropTypes from 'prop-types';
 
 class Todos extends Component {
   render() {
-    return (
+    return this.props.todos.map((todo) => (
+      //todo is passed to TodoItem as a prop! needs a unique key which we created when we made our array of objects
       <div>
-          <h1>Todos</h1>
+          <TodoItem key={todo.id} todo={todo} />
       </div>
-    );
+    ));
   }
+}
+
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired
 }
 
 export default Todos;
